54e7df8c116eba1fcfad23e309fd945776e45612
Robin DUDEK <rarkalon@hotmail.com>
2016-03-11 09:14:06 +0100

    copying actual java code

---
+++ app/src/main/java/com/bachelor/robin/api_twitter/launcher/LauncherActivity.java
@@ -0,0 +1,7 @@
+package com.bachelor.robin.api_twitter.launcher;
+
+/**
+ * Created by rorod on 10/03/2016.
+ */
+public class LauncherActivity {
+}
--- app/src/main/java/com/bachelor/robin/api_twitter/main/MainActivity.java
+++ app/src/main/java/com/bachelor/robin/api_twitter/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bachelor.robin.api_twitter;
+package com.bachelor.robin.api_twitter.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bachelor.robin.api_twitter.R;
 
 public class MainActivity extends AppCompatActivity {
 
